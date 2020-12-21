bf8a8af683a38c88eb61143dc5f4853dd1ede00f
sandro sviben <sandro.sviben@axilis.com>
2020-12-21 12:03:21 +0100

    adds Settings and Enums classes

---
+++ UI/Helpers/Enums.cs
@@ -0,0 +1,19 @@
+﻿namespace UI.Helpers
+{
+    class Enums
+    {
+        public enum ExecutionEnvironmentType
+        {
+            SILENT,
+            STAGE,
+            QA
+        }
+        public enum UserType
+        {
+            SPORT,
+            LOTTO,
+            GAMES,
+            NONBETTING
+        }
+    }
+}
