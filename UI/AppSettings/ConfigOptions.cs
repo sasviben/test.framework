7ba66b9c31c33ede99f4eb4dd0efaec16d699d12
sandro sviben <sandro.sviben@axilis.com>
2020-12-21 14:15:53 +0100

    adds config options class

---
+++ UI/AppSettings/ConfigOptions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UI.Configuration;
+
+namespace UI.AppSettings
+{
+    class ConfigOptions
+    {
+        public string HomepageUrl { get; set; }
+        public string HomepageUrlFF { get; set; }
+        public string Browser { get; set; }
+        public string CustomerSessionAPI { get; set; }
+        public string PlayerBalanceAPI { get; set; }
+        public string Domain { get; set; }
+        public string ClientSourceType { get; set; }
+        public List<Credentials> CustomerCredentials { get; set; }
+    }
+}
