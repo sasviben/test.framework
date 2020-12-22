f859ef415c755c6f86b6e15e508590935b5bff43
sandro sviben <sandro.sviben@axilis.com>
2020-12-22 15:07:17 +0100

    adds Waiter class

---
+++ UI/Helpers/Waiters.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace UI.Helpers
+{
+    public static class Waiters
+    {
+        /// <summary>
+        ///     Checks if element is visible in DOM by its locator.
+        /// </summary>
+        /// <param name="locator">
+        ///     Locator pointing to the web element.
+        /// </param>
+        /// <param name="driver">
+        ///     Instance of Selenium IWebDriver.
+        /// </param>
+        /// <param name="seconds">
+        ///     Time to wait for element become visible.
+        ///     Default: 10 seconds
+        /// </param>
+        /// <returns>
+        ///     True if element is visible.
+        ///     False if element isn't visible in a specified timeframe.
+        /// </returns>
+        public static bool IsElementVisible(By locator, IWebDriver driver, int seconds = 10)
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
+            try
+            {
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(locator));   
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
