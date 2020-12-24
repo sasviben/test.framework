907b75cc6080777f0d24846821bce58b94c5a7cd
sandro sviben <sandro.sviben@axilis.com>
2020-12-24 09:11:16 +0100

    adds locators for the login web form

---
+++ UI/Locators/LoginFormLOC.cs
@@ -0,0 +1,12 @@
+﻿using OpenQA.Selenium;
+
+namespace UI.Locators
+{
+    class LoginFormLOC
+    {
+        public static By ButtonSubmit { get; } = By.CssSelector("#login-modal-submit");
+        public static By FieldUsername { get; } = By.CssSelector(".input__field-box .input__field");
+        public static By FieldPassword { get; } = By.CssSelector(".password-input .input__field");
+
+    }
+}
