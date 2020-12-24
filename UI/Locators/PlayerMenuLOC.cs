62b8717bd2c8e07050c0f14880c9f63abd2f3aea
sandro sviben <sandro.sviben@axilis.com>
2020-12-24 09:11:39 +0100

    adds locators for player menu modal

---
+++ UI/Locators/PlayerMenuLOC.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+
+namespace UI.Locators
+{
+    class PlayerMenuLOC
+    {
+        public static By Balance { get; } = By.CssSelector(".balance");
+        public static By NavigationAccountOverview { get; } = By.CssSelector(".e2e_player-account");
+        public static By NavigationTickets { get; } = By.CssSelector(".e2e_player-tickets");
+        public static By NavigationTransactionsHistory { get; } = By.CssSelector(".e2e_player-history");
+        public static By ButtonLogout { get; } = By.CssSelector("#nav-account-logout");
+    }
+}
