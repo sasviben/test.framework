199189ffc61c4b76d14261a1ad28f735a533831a
sandro sviben <sandro.sviben@axilis.com>
2020-12-24 09:13:06 +0100

    adds login steps implementation

---
+++ UI/Steps/PlayerProfileSteps.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+using UI.Objects;
+
+namespace UI.Steps
+{
+    [Binding]
+    class PlayerProfileSteps
+    {
+        public PlayerProfileSteps(IWebDriver webDriver)
+        {
+            _driver = webDriver;
+            _login = new LoginObject(_driver);
+        }
+
+        private readonly IWebDriver _driver;
+        private readonly LoginObject _login;
+
+
+        #region Actions
+        [When(@"the player tries to log in with valid credentials")]
+        public void WhenThePlayerTriesToLogInWithValidCredentials()
+        {
+            _login.OpenLoginModal();
+            _login.Login();
+        }
+
+        #endregion
+
+        #region Assertions
+        [Then(@"the player should be logged in")]
+        public void ThenThePlayerShouldBeLoggedIn()
+        {
+            _login.IsTheUserLoggedIn();
+        }
+
+        #endregion
+    }
+}
