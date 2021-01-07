e81aa4dfb8f4766405709087ab052e73efbb44a2
sandro sviben <sandro.sviben@axilis.com>
2021-01-07 07:39:33 +0100

    removes Models map

--- UI/Locators/BetslipLOC.cs
+++ UI/Locators/BetslipLOC.cs
@@ -5,6 +5,7 @@ namespace UI.Locators
     class BetslipLOC
     {
         public static By EventValidationMessage { get; } = By.CssSelector(".selection-row__error-message");
+        public static By ValidationMessage { get; } = By.CssSelector(".betslip-error");
         public static By EventsCount { get; } = By.CssSelector(".betslip__controls-events-info-number");
         public static By PotentialWin { get; } = By.CssSelector(".betslip__potential-gain-amount");
         public static By OddTotal { get; } = By.CssSelector(".betslip-stake__total-quota-number");
@@ -12,6 +13,7 @@ namespace UI.Locators
         public static By RowEvent { get; } = By.CssSelector(".selection-row");
         public static By CheckboxMarket { get; } = By.CssSelector(".selection-row__selection-switch");
         public static By InputFieldStake { get; } = By.CssSelector(".input__field");
+        public static By Spinner { get; } = By.CssSelector(".betslip__purchase-loading-copy");
         public static By ButtonDeleteEvent { get; } = By.CssSelector(".betslip-body__side .icon-delete");
         public static By ButtonCopy { get; } = By.CssSelector(".icon-duplicate");
         public static By ButtonMultiBetslipSwitch { get; } = By.CssSelector(".multi-betslip-controls__pill");
@@ -20,7 +22,7 @@ namespace UI.Locators
         public static By ButtonSimpleTicket { get; } = By.CssSelector(".betslip-header .e2e-binary-switcher-first");
         public static By ButtonSystemTicket { get; } = By.CssSelector(".betslip-header .e2e-binary-switcher-second");
         public static By ButtonOnlineTicket { get; } = By.CssSelector(".betslip-footer .e2e-binary-switcher-first");
-        public static By ButtonOfflineTicket { get; } = By.CssSelector(".betslip-footer .e2e-binary-switcher-second");
+        public static By ButtonAgencyTicket { get; } = By.CssSelector(".betslip-footer .e2e-binary-switcher-second");
         public static By ButtonTicketPreview { get; } = By.CssSelector(".betslip__ticket-preview");
         public static By ButtonStakePicker { get; } = By.CssSelector(".betslip-stake__picker");
 
